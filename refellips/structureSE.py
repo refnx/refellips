8d9686d80359ba588aa86361e281c642a271c99c
haydenrob <40317193+haydenrob@users.noreply.github.com>
2022-02-18 14:57:57 +1100

    Updated docstrings and license

--- refellips/structureSE.py
+++ refellips/structureSE.py
@@ -1,8 +1,40 @@
 """
-Created on Mon Sep 21 11:10:38 2020.
+BSD 3-Clause License
 
-@author: Isaac
+Copyright (c) 2020-2022,
+Hayden Robertson (University of Newcastle)
+Isaac Gresham (University of Sydney)
+Andrew Nelson (ANSTO)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
+
+# -*- coding: utf-8 -*-
+
 from refnx.reflect.structure import Scatterer
 from refnx.analysis import Parameters, Parameter, possibly_create_parameter
 import numpy as np
@@ -109,10 +141,6 @@ class RI(Scatterer):
             self.B = possibly_create_parameter(B, name=f"{name} - cauchy B")
             self.C = possibly_create_parameter(C, name=f"{name} - cauchy C")
             self._parameters.extend([self.A, self.B, self.C])
-
-        # The RI needs access to the model to calculate the refractive index.
-        # Can't think of a better way of doing this
-        # reflect_modelSE is going to auto-link this when its called.
 
     @property
     def real(self):
--- tools/plottools.py
+++ tools/plottools.py
@@ -8,14 +8,23 @@ Created on Tue Aug 24 08:40:16 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_ellipsdata (ax, data=None, model=None, objective=None, xaxis='aoi', plot_labels=True, legend=True):
+
+def plot_ellipsdata(
+    ax,
+    data=None,
+    model=None,
+    objective=None,
+    xaxis="aoi",
+    plot_labels=True,
+    legend=True,
+):
     """
     Plots delta and psi values as a function of wavelength or angle of incidence.
     Compatible with VASE.
-    
+
     Must pass an axis object for plot_ellipsdata to plot on. To create an axis
     object:
-        
+
         fig, ax = plt.subplots()
 
     Parameters
@@ -42,39 +51,41 @@ def plot_ellipsdata (ax, data=None, model=None, objective=None, xaxis='aoi', plo
     None.
 
     """
-    
+
     if objective != None:
-        assert data == None and model == None, "If objective is supplied, model and data should not be passed"
+        assert (
+            data == None and model == None
+        ), "If objective is supplied, model and data should not be passed"
         data = objective.data
         model = objective.model
     elif model != None:
         assert data != None, "If you supply a model, you must also supply data"
     else:
         assert data != None, "must supply at least one of data, model or objective"
-        
-    assert xaxis == 'aoi' or xaxis == 'wavelength', "xaxis must be 'aoi' or 'wavelength"
-        
+
+    assert xaxis == "aoi" or xaxis == "wavelength", "xaxis must be 'aoi' or 'wavelength"
+
     axt = ax.twinx()
     data.mask = np.ones_like(data.mask, dtype=bool)
 
-    if xaxis == 'aoi':
+    if xaxis == "aoi":
         unique_wavs = np.unique(data.wav)
-        aois = np.linspace(np.min(data.aoi)-5, np.max(data.aoi)+5)
+        aois = np.linspace(np.min(data.aoi) - 5, np.max(data.aoi) + 5)
         x = data.aoi
-        xlab = 'AOI, °'
+        xlab = "AOI, °"
 
         if model != None:
             for wav in unique_wavs:
-                model.wav=wav
+                model.wav = wav
                 psis, deltas = model(aois)
-                ax.plot(aois, psis, color='r')
-                axt.plot(aois, deltas, color='b')
+                ax.plot(aois, psis, color="r")
+                axt.plot(aois, deltas, color="b")
 
-    elif xaxis == 'wavelength':
+    elif xaxis == "wavelength":
         unique_aois = np.unique(data.aoi)
-        wavs = np.linspace(np.min(data.wav)-50, np.max(data.wav)+50)
+        wavs = np.linspace(np.min(data.wav) - 50, np.max(data.wav) + 50)
         x = data.wav
-        
+
         if model != None:
             for u in unique_aois:
                 psis = []
@@ -84,29 +95,31 @@ def plot_ellipsdata (ax, data=None, model=None, objective=None, xaxis='aoi', plo
                     psi, delta = model([u])
                     psis.append(psi)
                     deltas.append(delta)
-                
-            ax.plot(wavs, psis, color='r')
-            axt.plot(wavs, deltas, color='b')
-            xlab = 'Wavelength, nm'
 
-    p=ax.scatter(x, data.psi, color='r')
-    d=axt.scatter(x, data.delta, color='b')
+            ax.plot(wavs, psis, color="r")
+            axt.plot(wavs, deltas, color="b")
+            xlab = "Wavelength, nm"
 
-    ax.legend(handles=[p,d], labels=['Psi', 'Delta'])
-        
+    p = ax.scatter(x, data.psi, color="r")
+    d = axt.scatter(x, data.delta, color="b")
+
+    ax.legend(handles=[p, d], labels=["Psi", "Delta"])
+
     if plot_labels:
-        ax.set(ylabel='Psi', xlabel=xlab)
-        axt.set(ylabel='Delta')
+        ax.set(ylabel="Psi", xlabel=xlab)
+        axt.set(ylabel="Delta")
 
 
-def plot_structure(ax, objective=None, structure=None, reverse_structure=False, plot_labels=True):
+def plot_structure(
+    ax, objective=None, structure=None, reverse_structure=False, plot_labels=True
+):
     """
     Plots refractive index as a function of distance from the substrate.
     Compatible with VASE.
-    
+
     Must pass an axis object for plot_structure to plot on. To create an axis
     object:
-        
+
         fig, ax = plt.subplots()
 
     Parameters
@@ -130,23 +143,24 @@ def plot_structure(ax, objective=None, structure=None, reverse_structure=False, 
 
     """
     if objective != None:
-        assert structure == None, "you must supply either an objective or structure, not both"
+        assert (
+            structure == None
+        ), "you must supply either an objective or structure, not both"
         structure = objective.model.structure
         wavelengths = np.unique(objective.data.wav)
         model = objective.model
-    else: 
+    else:
         assert structure != None, "you must supply either an objective or structure"
         wavelengths = [658]
         model = False
-        
+
     if len(wavelengths) > 1:
-        colors = plt.cm.viridis(np.linspace(0,1,len(wavelengths)))
-        alpha=0.5
+        colors = plt.cm.viridis(np.linspace(0, 1, len(wavelengths)))
+        alpha = 0.5
     else:
-        colors=['k']
-        alpha=1
-    
-    
+        colors = ["k"]
+        alpha = 1
+
     structure.reverse_structure = reverse_structure
 
     for wav, col in zip(wavelengths, colors):
@@ -156,10 +170,10 @@ def plot_structure(ax, objective=None, structure=None, reverse_structure=False, 
             else:
                 x.sld.set_wav = wav
 
-        ax.plot(*structure.sld_profile(), color=col, alpha=alpha, label=f'{wav} nm')
+        ax.plot(*structure.sld_profile(), color=col, alpha=alpha, label=f"{wav} nm")
 
     structure.reverse_structure = False
 
     if plot_labels:
-        ax.set(ylabel='Refractive index', xlabel='Distance from substrate (Å)')
+        ax.set(ylabel="Refractive index", xlabel="Distance from substrate (Å)")
         ax.legend(ncol=2, fontsize=7, frameon=False)
