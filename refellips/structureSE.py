9926758ca3a02075f9b76b18aac9f1569d30e772
Andrew Nelson <andyfaff@gmail.com>
2022-02-22 14:41:49 +1100

    MAINT: amend RI for Scatterer

--- refellips/structureSE.py
+++ refellips/structureSE.py
@@ -59,8 +59,6 @@ class RI(Scatterer):
     Parameters
     ----------
     value : tuple, string
-        Scattering length density of a material.
-        Units (10**-6 Angstrom**-2)
     A : float or parameter
         Cauchy parameter A. If not none RI will use the cauchy model.
         Default None.
@@ -68,28 +66,17 @@ class RI(Scatterer):
         Cauchy parameter B in um^2. Default 0.
     C : float or parameter
         Cauchy parameter C in um^4. Default 0.
+    wavelength : float
+        default wavelength for calculation (nm)
     name : str, optional
         Name of material.
 
     Notes
     -----
-    An SLD object can be used to create a Slab:
-
-    >>> # an SLD object representing Silicon Dioxide
-    >>> sio2 = SLD(3.47, name='SiO2')
-    >>> # create a Slab of SiO2 20 A in thickness, with a 3 A roughness
-    >>> sio2_layer = sio2(20, 3)
-
-    The SLD object can also be made from a complex number, or from Parameters
-
-    >>> sio2 = SLD(3.47+0.01j)
-    >>> re = Parameter(3.47)
-    >>> im = Parameter(0.01)
-    >>> sio2 = SLD(re)
-    >>> sio2 = SLD([re, im])
+    An RI object can be used to create a Slab:
     """
 
-    def __init__(self, value=None, A=None, B=0, C=0, name=""):
+    def __init__(self, value=None, A=None, B=0, C=0, wavelength=658, name=""):
         if (
             type(value) is str and name == ""
         ):  # if there is no name get it from the path
@@ -97,13 +84,20 @@ class RI(Scatterer):
 
         super(RI, self).__init__(name=name)
 
+        # attribute required by Scatterer for energy dispersive calculations
+        # to work
+        self.dispersive = True
+        self.wavelength = wavelength
+
+        # _wav is only set if a wavelength dependent dispersion curve is loaded
+        self._wav = None
+
         assert np.logical_xor(
             value is None, A is None
         ), "Supply either values or cauchy parameters"
 
         if value is not None:
             if type(value) is str:
-
                 try:
                     self._wav, self._RI, self._EC = np.loadtxt(
                         value, skiprows=1, delimiter=",", encoding="utf8"
@@ -117,24 +111,22 @@ class RI(Scatterer):
                         encoding="utf8",
                     ).T
                     self._EC = np.zeros_like(self._wav)
-
+                # convert wavelength from um to nm
+                self._wav = self._wav * 1000
             elif len(value) == 2:
                 self._RI, self._EC = value
-                self._wav = None
             elif len(value) == 3:
+                # this is if you have an (3, N) array or tuple specifying
+                # wavelength, RI, extinction coef.
+                # wavelength assumed to be in *nm*
                 self._wav, self._RI, self._EC = value
             else:
                 raise TypeError("format not recognised")
-            # convert wavelength from um to nm
-            self._wav = self._wav * 1000
         else:
-            self._wav = None
+            self.wavelength = wavelength
             self._RI = None
             self._EC = None
 
-        self.model = None
-        self.set_wav = None
-        self._default_wav = 658
         self._parameters = Parameters(name=name)
 
         if A is not None:
@@ -144,61 +136,51 @@ class RI(Scatterer):
             self._parameters.extend([self.A, self.B, self.C])
 
     @property
-    def real(self):
-        """Refractive index, n."""
-
-        if self.model is not None:
-            wavelength = self.model.wav
-        elif self.set_wav is not None:
-            wavelength = self.set_wav
-        else:
-            wavelength = self._default_wav
-            warnings.warn("Using default wavelength (model not linked)")
-
-        if np.any(self._wav):
-            # TODO - raise a warning if the wavelength supplied is outside the
-            # wavelength range covered by the data file.
-
-            return Parameter(np.interp(wavelength, self._wav, self._RI))
-
-        elif self.A is not None:
-            return Parameter(
-                self.A.value
-                + (self.B.value * 1000**2) / (wavelength**2)
-                + (self.C.value**1000**4) / (wavelength**4)
-            )
-        else:
-            return Parameter(value=self._RI)
-
-    @property
-    def imag(self, wavelength=None):
-        """Extinction coefficent, k."""
-
-        if self.model is not None:
-            wavelength = self.model.wav
-        elif self.set_wav is not None:
-            wavelength = self.set_wav
-        else:
-            wavelength = self._default_wav
-            warnings.warn("Using default wavelength (model not linked)")
-
-        if np.any(self._wav):
-            # TODO - raise a warning if the wavelength supplied is outside the
-            # wavelength range covered by the data file.
-
-            return Parameter(np.interp(wavelength, self._wav, self._EC))
-        elif self.A is not None:
-            return Parameter(0)
-        else:
-            return Parameter(value=self._EC)
-
-    @property
     def parameters(self):
         return self._parameters
 
     def __repr__(self):
-        return str(f"n: {self.real.value}, k: {self.imag.value}")
+        ri = complex(self)
+        return str(f"n: {ri.real}, k: {ri.imag}")
 
     def __complex__(self):
-        sldc = complex(self.real.value, self.imag.value)
-        return sldc
+        """
+        The refractive index and extinction coefficient
+        """
+        return self.complex(None)
+
+    def complex(self, wavelength):
+        """
+        Calculate a complex RI
+
+        Parameters
+        ----------
+        wavelength : float
+            wavelength of light in nm
+
+        Returns
+        -------
+        RI : complex
+            refractive index and extinction coefficient
+        """
+        # just in case wavelength is None
+        wav = wavelength or self.wavelength
+
+        if np.any(self._wav):
+            # return a wavelength from a dispersion curve
+            # TODO - raise a warning if the wavelength supplied is outside the
+            # wavelength range covered by the data file.
+            ri_real = np.interp(wav, self._wav, self._RI)
+            ri_imag = np.interp(wav, self._wav, self._EC)
+            return ri_real + 1J*ri_imag
+
+        elif self.A is not None:
+            # TODO query about the cauchy value calculation
+            real = (
+                self.A.value
+                + (self.B.value * 1000**2) / (wav**2)
+                + (self.C.value * 1000**4) / (wav**4)
+            )
+            return real + 1J*0.0
+        else:
+            return complex(self._RI, self._EC)
