9e879ee77021fc21a18aa0f1fe0f9f7e12ba903b
Andrew Nelson <andyfaff@gmail.com>
2022-03-15 18:23:51 +1100

    ENH: ema (#33)
    * ENH: ema
    
    * ENH: ema
    
    * STY
    
    * MAINT: remove method

--- refellips/structureSE.py
+++ refellips/structureSE.py
@@ -35,20 +35,20 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 # -*- coding: utf-8 -*-
-
-from refnx.reflect.structure import Scatterer
-from refnx.analysis import Parameters, Parameter, possibly_create_parameter
 import numpy as np
 import os
 import warnings
+
+from refnx.reflect.structure import Scatterer, Structure, Slab, Component
+from refnx.analysis import Parameters, Parameter, possibly_create_parameter
 
 
 class RI(Scatterer):
     """
     Object representing a materials wavelength-dependent refractive index.
 
-    An issue is that optical parameters are supplied in units of micro
-    meters ('cause thats what seems to be used in refractive index repos and
+    An issue is that optical parameters are supplied in units of micrometers
+    ('cause thats what seems to be used in refractive index repos and
     cauchy models), the wavelength of the incident radiation is supplied in
     nanometers (thats typical) and the fitting is done in angstroms. Very
     unpleasant.
@@ -84,7 +84,7 @@ class RI(Scatterer):
 
     Notes
     -----
-    An RI object can be used to create a Slab:
+    An RI object can be used to create a Slab
     """
 
     def __init__(
@@ -191,3 +191,222 @@ class RI(Scatterer):
             return real + 1j * 0.0
         else:
             return self._RI + 1j * self._EC
+
+    def __call__(self, thick=0, rough=0, vfsolv=0):
+        """
+        Create a :class:`SlabSE`.
+
+        Parameters
+        ----------
+        thick: refnx.analysis.Parameter or float
+            Thickness of slab in Angstrom
+        rough: refnx.analysis.Parameter or float
+            Roughness of slab in Angstrom
+        vfsolv: refnx.analysis.Parameter or float
+            Volume fraction of water in slab
+
+        Returns
+        -------
+        slab : SlabSE
+            The newly made Slab.
+
+        """
+        return SlabSE(thick, self, rough, name=self.name, vfsolv=vfsolv)
+
+
+class ComponentSE(Component):
+    """
+    A base class for describing the structure of a subset of an interface.
+
+    Parameters
+    ----------
+    name : str, optional
+        The name associated with the Component
+
+    Notes
+    -----
+    By setting the `Component.interfaces` property one can control the
+    type of interfacial roughness between all the layers of an interfacial
+    profile.
+    """
+
+    def __or__(self, other):
+        """
+        OR'ing SlabSE can create a :class:`StructureSE`.
+
+        Parameters
+        ----------
+        other: refellips.StructureSE, refnx.reflect.Component
+            Combines with this component to make a Structure
+
+        Returns
+        -------
+        s: refellips.StructureSE
+            The created Structure
+
+        """
+        # c = self | other
+        p = StructureSE()
+        p |= self
+        p |= other
+        return p
+
+    def __mul__(self, n):
+        """
+        MUL'ing components makes them repeat.
+
+        Parameters
+        ----------
+        n: int
+            How many times you want to repeat the Component
+
+        Returns
+        -------
+        s: refellipse.StructureSE
+            The created Structure
+        """
+        # convert to integer, should raise an error if there's a problem
+        n = operator.index(n)
+        if n < 1:
+            return StructureSE()
+        elif n == 1:
+            return self
+        else:
+            s = StructureSE()
+            s.extend([self] * n)
+            return s
+
+
+class SlabSE(ComponentSE, Slab):
+    """
+    A slab component has uniform RI over its thickness.
+
+    Parameters
+    ----------
+    thick : refnx.analysis.Parameter or float
+        thickness of slab (Angstrom)
+    sld : :class:`refnx.reflect.Scatterer`, complex, or float
+        (complex) refractive index of film
+    rough : refnx.analysis.Parameter or float
+        roughness on top of this slab (Angstrom)
+    name : str
+        Name of this slab
+    vfsolv : refnx.analysis.Parameter or float
+        Volume fraction of solvent [0, 1]
+    interface : {:class:`Interface`, None}, optional
+        The type of interfacial roughness associated with the Slab.
+        If `None`, then the default interfacial roughness is an Error
+        function (also known as Gaussian roughness).
+    """
+
+
+class StructureSE(Structure):
+    """
+    Represents the interfacial Structure of an Ellipsometry sample.
+    Successive Components are added to the Structure to construct the
+    interface.
+    """
+
+    # the only reason to have this class is to have the correct overall_sld
+    # method
+    def __init__(self, *args, ema="linear", **kwds):
+        super().__init__(*args, **kwds)
+        self.ema = ema
+
+    def __ior__(self, other):
+        """
+        Build a structure by `IOR`'ing Structures/Components/SLDs.
+
+        Parameters
+        ----------
+        other: :class:`StructureSE`, :class:`ComponentSE`, :class:`RI`
+            The object to add to the structure.
+        """
+        # self |= other
+        if isinstance(other, ComponentSE):
+            self.append(other)
+        elif isinstance(other, StructureSE):
+            self.extend(other.data)
+        elif isinstance(other, Scatterer):
+            slab = other(0, 0)
+            self.append(slab)
+        else:
+            raise ValueError()
+
+        return self
+
+    def __or__(self, other):
+        """
+        Build a structure by `OR`'ing Structures/Components/SLDs.
+
+        Parameters
+        ----------
+        other: :class:`StructureSE`, :class:`ComponentSE`, :class:`SLD`
+            The object to add to the structure.
+        """
+        # c = self | other
+        p = StructureSE()
+        p |= self
+        p |= other
+        return p
+
+    def overall_sld(self, slabs, solvent):
+        """
+        Calculates the overall refractive index of the material and solvent RI
+        in a layer.
+
+        Parameters
+        ----------
+        slabs : np.ndarray
+            Slab representation of the layers to be averaged.
+        solvent : complex or RI
+            RI of solvating material.
+
+        Returns
+        -------
+        averaged_slabs : np.ndarray
+            the averaged slabs.
+
+        Notes
+        -----
+        This method is called `overall_sld` to take advantage of inheritance, i.e.
+        we don't have to override the `slabs` method. It definitely does calculate
+        the overall RI.
+        """
+        solv = solvent
+        if isinstance(solvent, Scatterer):
+            solv = solvent.complex(self.wavelength)
+
+        return overall_RI(slabs, solv, ema=self.ema)
+
+
+def overall_RI(slabs, solvent, ema="linear"):
+    """
+    Calculates the overall refractive index of the material and solvent RI
+    in a layer.
+
+    Parameters
+    ----------
+    slabs : np.ndarray
+        Slab representation of the layers to be averaged.
+    solvent : complex or RI
+        RI of solvating material.
+    ema : {'linear'}
+        Specifies how refractive indices are mixed together
+
+    Returns
+    -------
+    averaged_slabs : np.ndarray
+        the averaged slabs.
+    """
+    if ema == "linear":
+        slabs[..., 1:3] = slabs[..., 1:3] ** 2
+        slabs[..., 1:3] *= (1 - slabs[..., 4])[..., np.newaxis]
+
+        slabs[..., 1] += solvent.real**2 * slabs[..., 4]
+        slabs[..., 2] += solvent.imag**2 * slabs[..., 4]
+        slabs[..., 1:3] = np.sqrt(slabs[..., 1:3])
+    else:
+        raise RuntimeError("No other method of mixing is known")
+
+    return slabs
