9b2cf08741fb288d745444032210c3d04d42cc02
Andrew Nelson <andyfaff@gmail.com>
2022-03-18 14:51:08 +1100

    TST: test2

--- refellips/tests/test_refellips.py
+++ refellips/tests/test_refellips.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os.path
+from os.path import join as pjoin
 from refellips import (
     RI,
     Cauchy,
@@ -11,18 +12,18 @@ from refellips import (
 )
 from numpy.testing import assert_allclose
 
+pth = os.path.dirname(os.path.abspath(__file__))
 
-def test_bare_against_wvase():
-    # Check bare interface ellipsometry calcualtion against wvase
-    pth = os.path.dirname(os.path.abspath(__file__))
 
-    dname = os.path.join(pth, "TestData_bareSI.txt")
+def test_bare_against_wvase1():
+    # Check bare interface ellipsometry calculation against wvase
+    dname = pjoin(pth, "TestData_bareSI.txt")
     data = DataSE(data=dname)
 
-    _f = os.path.join(pth, "../materials/silicon.csv")
+    _f = pjoin(pth, "../materials/silicon.csv")
     si = RI(_f)
 
-    _f = os.path.join(pth, "../materials/void.csv")
+    _f = pjoin(pth, "../materials/void.csv")
     void = RI(_f)
 
     struc = void() | si()
@@ -41,12 +42,28 @@ def test_bare_against_wvase():
     _ = struc.ri_profile()
 
 
+def test_Cauchy_against_wvase2():
+    # Check Cauchy model against wvase
+    A = 1.47
+    B = 0.00495
+    C = 0
+
+    cauchy = Cauchy(A=A, B=B, C=C)
+
+    wvase_output = np.loadtxt(pjoin(pth, "Cauchynk_fromWVASE.txt"))
+    wavs = wvase_output[:, 0]
+
+    refin = A + B / ((wavs / 1000) ** 2) + C / ((wavs / 1000) ** 4)
+
+    refellips_RI = cauchy.complex(wavs)
+
+    assert_allclose(refellips_RI, refin)
+    assert_allclose(refellips_RI, wvase_output[:, 1], rtol=1e-6)
+
+
 def test_refellips_against_wvase3():
     # Three layer stack(cauchy & from file) compared to wvase
     # Also tests behaviour of Delta around 180°
-
-    pth = os.path.dirname(os.path.abspath(__file__))
-
     dname = os.path.join(pth, "TestData_cauchy.txt")
     data = DataSE(data=dname)
 
