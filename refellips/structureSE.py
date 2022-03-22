700295df132beb79a624168077992ca69308ec3e
haydenrob <40317193+haydenrob@users.noreply.github.com>
2022-03-22 17:21:49 +1100

    MAINT

--- refellips/structureSE.py
+++ refellips/structureSE.py
@@ -649,7 +649,7 @@ class StructureSE(Structure):
         >>> StructureSE.depolarisation_factor = 1/3
         b = e_h * ((1 - vf) - depolarisation_factor) + e_i * (vf - depolarisation_factor)
         e_BG = (b + np.sqrt(b**2 - 4 * (depolarisation_factor - 1) *
-                                 (2 * vf * e_h * e_i * depolarisation_factor
+                                 (vf * e_h * e_i * depolarisation_factor
                                   ))) / (2 * (1 - depolarisation_factor))
         """
 
@@ -969,7 +969,7 @@ def overall_RI(slabs, solvent, ema="linear", depolarisation_factor=1 / 3):
                     b**2
                     - 4
                     * (depolarisation_factor - 1)
-                    * (2 * E * solvent**2 * depolarisation_factor)
+                    * (E * solvent**2 * depolarisation_factor)
                 )
             ) / (2 * (1 - depolarisation_factor))
             E = np.where(np.isfinite(v), v, 0)
