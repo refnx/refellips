95d32a2716eb77fb8193038886386028e4a5240b
Andrew Nelson <andyfaff@gmail.com>
2022-08-28 22:17:33 +1000

    STY

--- refellips/__init__.py
+++ refellips/__init__.py
@@ -17,7 +17,7 @@ from .dispersion import (
     TaucLorentz,
     load_material,
     materials,
-    nm_eV_conversion
+    nm_eV_conversion,
 )
 from .dataSE import DataSE
 from .reflect_modelSE import ReflectModelSE
